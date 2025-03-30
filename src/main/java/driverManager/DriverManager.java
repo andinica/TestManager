b5e2f8c17e7c00a11a141041bbf0fbca654b53f5
adinica <andreic.dinica@gmail.com>
2025-03-30 15:00:17 +0300

    format

--- src/main/java/driverManager/DriverManager.java
+++ src/main/java/driverManager/DriverManager.java
@@ -1,125 +1,141 @@
 package driverManager;
 
-import lombok.SneakyThrows;
+import static utils.ProcessUtils.executeCommand;
+import static utils.ProcessUtils.updatePath;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.InvalidPropertiesFormatException;
 import java.util.List;
 import java.util.Properties;
-
-import static utils.ProcessUtils.executeCommand;
-import static utils.ProcessUtils.updatePath;
+import lombok.SneakyThrows;
+import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
-    private Properties appiumProperties;
-    private Properties driverProperties;
-    private Properties webDriverProperties;
-    private String appiumPropertiesFilePath = "src/main/resources/properties/driverCapabilities.properties";
-    private String driverPropertiesFilePath = "src/main/resources/properties/driverCapabilities.properties";
-    private String webDriverPropertiesFilePath = "src/main/resources/properties/driverCapabilities.properties";
+  private Properties appiumProperties;
+  private Properties driverProperties;
+  private Properties webDriverProperties;
+  private String appiumPropertiesFilePath =
+      "src/main/resources/properties/driverCapabilities.properties";
+  private String driverPropertiesFilePath =
+      "src/main/resources/properties/driverCapabilities.properties";
+  private String webDriverPropertiesFilePath =
+      "src/main/resources/properties/driverCapabilities.properties";
+  private WebDriver webDriver;
 
-    public DriverManager() {
-        loadProperties(driverProperties, driverPropertiesFilePath);
-        if(driverProperties.getProperty("ennvironment", "Web").equalsIgnoreCase("Mobile")) {
-            loadProperties(appiumProperties, appiumPropertiesFilePath);
-        } else {
-            loadProperties(webDriverProperties, webDriverPropertiesFilePath);
-        }
+  public DriverManager() {
+    loadProperties(driverProperties, driverPropertiesFilePath);
+    if (driverProperties.getProperty("ennvironment", "Web").equalsIgnoreCase("Mobile")) {
+      loadProperties(appiumProperties, appiumPropertiesFilePath);
+    } else {
+      loadProperties(webDriverProperties, webDriverPropertiesFilePath);
+    }
+  }
+
+  public static void main(String[] args) {
+    DriverManager manager = new DriverManager();
+    manager.setupAppium();
+    manager.setupSeleniumDriver();
+  }
+
+  private void loadProperties(Properties properties, String filePath) {
+    properties = new Properties();
+    try (FileInputStream fis = new FileInputStream(filePath)) {
+      properties.load(fis);
+    } catch (IOException e) {
+      throw new RuntimeException("Failed to load properties file: " + filePath, e);
+    }
+  }
+
+  private void installPackage(String packageName, String version) {
+    String command =
+        "npm install -g " + packageName + (version.equals("latest") ? "" : "@" + version);
+    executeCommand(command, "Installed " + packageName + " version: " + version);
+  }
+
+  public void setupAppium() {
+    String appiumVersion = appiumProperties.getProperty("appium.version", "");
+    boolean withDeviceFarm =
+        Boolean.parseBoolean(appiumProperties.getProperty("appium.withDeviceFarm", "false"));
+    String automationName = appiumProperties.getProperty("appium.automationName", "");
+
+    if (!isPackageInstalled("appium")) {
+      installPackage("appium", appiumVersion);
+      updatePath();
     }
 
-    private void loadProperties(Properties properties, String filePath){
-        properties = new Properties();
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            properties.load(fis);
-        } catch (IOException e) {
-            throw new RuntimeException("Failed to load properties file: " + filePath, e);
-        }
+    if (withDeviceFarm && !isPackageInstalled("@appium/plugin-device-farm")) {
+      installPackage("@appium/plugin-device-farm", "");
     }
 
-
-    private void installPackage(String packageName, String version) {
-        String command = "npm install -g " + packageName + (version.equals("latest") ? "" : "@" + version);
-        executeCommand(command, "Installed " + packageName + " version: " + version);
+    if ("uiautomator2".equalsIgnoreCase(automationName)
+        && !isPackageInstalled("appium-uiautomator2-driver")) {
+      installPackage("appium-uiautomator2-driver", "");
     }
 
-    public void setupAppium() {
-        String appiumVersion = appiumProperties.getProperty("appium.version", "");
-        boolean withDeviceFarm = Boolean.parseBoolean(appiumProperties.getProperty("appium.withDeviceFarm", "false"));
-        String automationName = appiumProperties.getProperty("appium.automationName", "");
+    if ("xcuitest".equalsIgnoreCase(automationName)
+        && !isPackageInstalled("appium-xcuitest-driver")) {
+      installPackage("appium-xcuitest-driver", "");
+    }
+  }
 
-        if (!isPackageInstalled("appium")) {
-            installPackage("appium", appiumVersion);
-            updatePath();
-        }
+  private boolean isPackageInstalled(String packageName) {
+    try {
+      Process process = Runtime.getRuntime().exec("npm list -g " + packageName);
+      process.waitFor();
+      return process.exitValue() == 0;
+    } catch (IOException | InterruptedException e) {
+      return false;
+    }
+  }
 
-        if (withDeviceFarm && !isPackageInstalled("@appium/plugin-device-farm")) {
-            installPackage("@appium/plugin-device-farm", "");
-        }
+  public void setupSeleniumDriver() {
+    String driverVersion = driverProperties.getProperty("version", "latest");
+    String driverName = driverProperties.getProperty("driver");
 
-        if ("uiautomator2".equalsIgnoreCase(automationName) && !isPackageInstalled("appium-uiautomator2-driver")) {
-            installPackage("appium-uiautomator2-driver", "");
-        }
+    installSeleniumDriver(driverName, driverVersion);
+    updatePath();
+  }
 
-        if ("xcuitest".equalsIgnoreCase(automationName) && !isPackageInstalled("appium-xcuitest-driver")) {
-            installPackage("appium-xcuitest-driver", "");
-        }
+  @SneakyThrows
+  private void installSeleniumDriver(String driverName, String version) {
+    List<String> supportedDrivers = List.of("chromedriver", "geckodriver");
+    if (supportedDrivers.contains(driverName.toLowerCase())) {
+      String browserVersion = getInstalledBrowserVersion(driverName);
+      String command =
+          "webdriver-manager update --"
+              + driverName
+              + (version.equals("latest")
+                  ? " --versions." + driverName + " " + browserVersion
+                  : " --versions." + driverName + " " + version);
+      executeCommand(
+          command,
+          "Installed "
+              + driverName
+              + " version: "
+              + (version.equals("latest") ? browserVersion : version));
+    } else {
+      throw new InvalidPropertiesFormatException(
+          String.format("Driver %s is not supported!", driverName));
+    }
+  }
+
+  private String getInstalledBrowserVersion(String driverName) throws IOException {
+    String command = "";
+    if (driverName.equalsIgnoreCase("chromedriver")) {
+      command = "google-chrome --version";
+    } else if (driverName.equalsIgnoreCase("geckodriver")) {
+      command = "firefox --version";
     }
 
-    private boolean isPackageInstalled(String packageName) {
-        try {
-            Process process = Runtime.getRuntime().exec("npm list -g " + packageName);
-            process.waitFor();
-            return process.exitValue() == 0;
-        } catch (IOException | InterruptedException e) {
-            return false;
-        }
+    java.io.BufferedReader reader =
+        new java.io.BufferedReader(
+            new java.io.InputStreamReader(new ProcessBuilder(command).start().getInputStream()));
+    String versionOutput = reader.readLine();
+
+    if (versionOutput != null && versionOutput.matches(".*\\d+(\\.\\d+)*.*")) {
+      return versionOutput.replaceAll("[^\\d.]", "").trim();
     }
-
-    public void setupSeleniumDriver() {
-        String driverVersion = driverProperties.getProperty("version", "latest");
-        String driverName = driverProperties.getProperty("driver");
-
-        installSeleniumDriver(driverName, driverVersion);
-        updatePath();
-    }
-
-    @SneakyThrows
-    private void installSeleniumDriver(String driverName, String version) {
-        List<String> supportedDrivers = List.of("chromedriver", "geckodriver");
-        if (supportedDrivers.contains(driverName.toLowerCase())) {
-            String browserVersion = getInstalledBrowserVersion(driverName);
-            String command = "webdriver-manager update --" + driverName +
-                    (version.equals("latest") ? " --versions." + driverName + " " + browserVersion : " --versions." + driverName + " " + version);
-            executeCommand(command, "Installed " + driverName + " version: " + (version.equals("latest") ? browserVersion : version));
-        } else {
-            throw new InvalidPropertiesFormatException(String.format("Driver %s is not supported!", driverName));
-        }
-    }
-
-    private String getInstalledBrowserVersion(String driverName) throws IOException {
-        String command = "";
-        if (driverName.equalsIgnoreCase("chromedriver")) {
-            command = "google-chrome --version";
-        } else if (driverName.equalsIgnoreCase("geckodriver")) {
-            command = "firefox --version";
-        }
-
-        Process process = Runtime.getRuntime().exec(command);
-        java.io.BufferedReader reader = new java.io.BufferedReader(new java.io.InputStreamReader(process.getInputStream()));
-        String versionOutput = reader.readLine();
-
-        if (versionOutput != null && versionOutput.matches(".*\\d+(\\.\\d+)*.*")) {
-            return versionOutput.replaceAll("[^\\d.]", "").trim();
-        }
-        return "latest";
-    }
-
-
-    public static void main(String[] args) {
-        DriverManager manager = new DriverManager();
-        manager.setupAppium();
-        manager.setupSeleniumDriver();
-    }
+    return "latest";
+  }
 }
-
